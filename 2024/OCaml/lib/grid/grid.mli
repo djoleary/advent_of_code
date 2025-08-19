b0c252f94ff38663617d0cef35de63f065804265
DJ O'Leary <dijitol@proton.me>
2025-08-19 10:12:29 +0200

    fix: remove invalid interface

--- 2024/OCaml/lib/grid/grid.mli
+++ 2024/OCaml/lib/grid/grid.mli
@@ -3,4 +3,4 @@ val to_grid : string list -> int -> Cell.t option
     of [tokens] is converted into rows with [width]. An empty list of [tokens]
     returns None. Requires: [width] is greater than 0 **)
 
-val print : Cell.t -> ()
+(* val print : Cell.t -> () *)
