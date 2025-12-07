cca606659bd0854cfe4f2668d3117ca42fe059b2
DJ O'Leary <dijitol@proton.me>
2025-12-07 03:44:36 +0100

    test(2025d4p1): add test case for solution of example

---
+++ 2025/ocaml/test/application/test_d04p01.ml
@@ -0,0 +1,24 @@
+open Application.D04p01
+
+let example =
+  {|..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.|}
+
+let test_input_to_matrix have want () =
+  Alcotest.(check int) "number of rolls of paper that are accessible" want
+  @@ solve have ()
+
+let () =
+  let open Alcotest in
+  run "D04p01"
+    [ ( "given-example"
+      , [test_case "full example" `Quick @@ test_input_to_matrix example 13] )
+    ]
